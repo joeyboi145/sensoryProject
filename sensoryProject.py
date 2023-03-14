cd5b2b45ed51e26ac80ad5fd684d85d050d966eb
Joe Martinez <joey@joes-air.lan>
2023-03-14 12:41:30 -0400

    unnecessary comments removed 2

--- sensoryProject.py
+++ sensoryProject.py
@@ -1,28 +1,31 @@
 import os
 import sys
 import errno
-
 import numpy as np
 import sounddevice as sd
 import cv2
 
 
-
 if sys.version[:1] != "3":
     raise Exception("Python version is not 3.X (atleast 3.0)")
 
+vid_FILE = None
+sound_File = None
+
+
+# Retrive User Video
 filename = input("\nPlease enter the name of the video file (i.e. timvid.mp4):\n")
 if os.path.exists(filename):
-    vid_FILE = os.path.abspath(filename)    # If so, store file's directory
+    vid_FILE = os.path.abspath(filename)
     print("\n\t'%s' file found!" % filename)
-else:                                       # If not, raise error
+else:
     raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), filename)
 
 
 # Check for file 'sensoryProject.txt' existance
 if os.path.exists("sensoryProject.txt"):
     sound_FILE = os.path.abspath("sensoryProject.txt")
-    f = open(sound_FILE, "r+")                          #   clears previous data 
+    f = open(sound_FILE, "r+")
     f.truncate(0)
 else:
     f = open("sensoryProject.txt", "x")
@@ -34,7 +37,7 @@ print("\n\t'sensoryProject.txt' CLEARED/INITIALIZED")
 
 
 
-# FUNCTION that frames captures a video
+# FUNCTION: frames captures a video
 #   Input:  Video file director
 #   Output: A GENERATOR object for the frames of a video
 def get_next_frame(filepath):
@@ -47,27 +50,27 @@ def get_next_frame(filepath):
     yield None
 
 
-# FUNCTION that loops through the frames of a video and applies the deepfry aspect depending on volume
+# FUNCTION: loops through the frames of a video and applies the deepfry aspect depending on volume
 #   Input:  Video file director
 def vidLoop(filepath):
-    volume = 1          # Mic input volume
+    volume = 1
     stream.start()      # Start audio stream (stream) on the first loop
     frames = get_next_frame(filepath)
     frame = frames.__next__()
 
     while(frames != None):
         # Retrieves mic volume input
-        f = open(sound_FILE, "r")    # Open 'deepfry_volume.txt' file in 'read mode'
+        f = open(sound_FILE, "r")
         try:
-            volume = f.readlines()[0]   # Tries to read the first line (lastest mic input volume)
-            volume = volume.strip()     # Strips EOL characters from mic input volume (as a string)
+            volume = f.readlines()[0]
+            volume = volume.strip()
         except:
-            print("ERROR: VOLUME READ FAILURE")    # If no value retrieved, print error 
+            print("ERROR: VOLUME READ FAILURE")
         f.close()
 
-        volume = float(volume)          # Converts mic input value from str to float value
+        volume = float(volume)
         if volume < 1:  volume = 1      # If mic input value is < 1, set = 1
-        #print(volume)                  # Print mic input value
+        #print(volume)
 
         # Increases the saturation of frame by mic input value (volume) 
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -88,20 +91,17 @@ def vidLoop(filepath):
     vidLoop(filename)
 
 
-# FUNCTION that processes the volumn input data
+# FUNCTION:  processes the volumn input data
 #   Input: the mic input data for volumn
 def frame_callback(indata, frames, time, status):
-    sensitivity = 10    # Determines the sensitivity of input volume
-                        # If you wanna change the sensitivity of the program,
-                        # Change this varible. 10 is default
+    sensitivity = 10    # 10 is default
 
-    # Takes the incoming data (indata) and converts it to volume data
+    # Takes the incoming data and converts it to volume data
     volume_norm = np.linalg.norm(indata) * sensitivity
 
-    f = open(sound_FILE, "r+")                  # Open 'deepfry_volume.txt' in 'write mode'         
-    f.write(str(volume_norm))                   # Stores volume data in file
+    f = open(sound_FILE, "r+")      
+    f.write(str(volume_norm))                   # Stores volume data in 'deepfry_volume.txt'
     f.close()
-   
     print("|" * int(volume_norm))               # Volume terminal visual
 
 
@@ -116,13 +116,10 @@ if __name__ == "__main__":
         callback = frame_callback)      # Volumn processing FUNCTION
     print("\tAUDIO STREAM INITIALIZED!\n\n")
 
-    # Creates window
-    cv2.namedWindow('Project - Deepfry', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('sensoryProject', cv2.WINDOW_NORMAL)
 
-    # Loops Video
     print("\tPLAYING VIDEO...\n")
     print("\tTo exit the program, press 'ESC' on the second window")
     vidLoop(filename)
     
-    # Destorys all windows
     cv2.destroyAllWindows()
