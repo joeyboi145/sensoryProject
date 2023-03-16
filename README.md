fa1b777ab16bc981eccd40ce81ac2c64d1404b9f
Joe Martinez <joey@joes-air.lan>
2023-03-16 00:34:30 -0400

    MAJOR RESTRUCTURE: Changed stream data processing from callback to block method, functional decomposition

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # sensoryProject
-An algorithm that increases the saturation of a video based on live microphone input. This program uses the the Numpy, Sounddevice, and CV2 Python libraries.
+An program that increases the saturation of a video based on live microphone input. This program uses the the Numpy, Sounddevice, CV2, and additional Python libraries.
 
 This project was done in collaboration with a Stony Brook Art Student. Credits to them for the video (timvid.mp4)
 
--- sensoryProject.py
+++ sensoryProject.py
@@ -6,41 +6,40 @@ import sounddevice as sd
 import cv2
 
 
-if sys.version[:1] != "3":
-    raise Exception("Python version is not 3.X (atleast 3.0)")
-
 vid_FILE = None
-sound_File = None
+stream = None
+CHUNK = 1024
 
 
-# Retrive user video
-filename = input("\nPlease enter the name of the video file (i.e. timvid.mp4):\n")
-if os.path.exists(filename):
-    vid_FILE = os.path.abspath(filename)
-    print("\n\t'%s' file found!" % filename)
-else:
-    raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), filename)
+# FUNCTION: Finds user video
+#   Output: relative path to video file
+def find_Video():
+    while True:
+        filename = input("Please enter the name of the video file (i.e. timvid.mp4):\n")
+        if os.path.exists(filename):
+            print("\n\t'%s' file found!" % filename)
+            return os.path.relpath(filename)
+        else:
+            print("\n\tFILE NOT FOUND. Please Try Again\n")
 
 
-# Check for file 'sensoryProject.txt' existance
-if os.path.exists("sensoryProject.txt"):
-    sound_FILE = os.path.abspath("sensoryProject.txt")
-    f = open(sound_FILE, "r+")
-    f.truncate(0)
-else:
-    f = open("sensoryProject.txt", "x")
-    sound_FILE = os.path.abspath("sensoryProject.txt")
-
-f.close()
-print("\n\t'sensoryProject.txt' CLEARED/INITIALIZED")
-
+# FUNCTION: Initiates sounddevice stream 
+#   Output: sounddevice input stream
+def init_stream():
+    print("\n\tAUDIO STREAM INITIALIZED!\n") 
+    return sd.InputStream(
+        samplerate = 44100,           # Standard Sample Rate
+        blocksize = CHUNK,            # Returns input at 512th frame
+        channels = 1,
+        device = sd.default.device,   # Default input device
+    )  
 
 
 # FUNCTION: frames captures a video
 #   Input:  Video file director
 #   Output: A GENERATOR object for the frames of a video
-def get_next_frame(filepath):
-    video = cv2.VideoCapture(filepath)
+def get_frames():
+    video = cv2.VideoCapture(vid_FILE)
     while video.isOpened():
         rete,frame = video.read()
         if rete:    yield frame
@@ -49,82 +48,58 @@ def get_next_frame(filepath):
     yield None
 
 
-# FUNCTION: retrieves volume data from 'sensoryProject.txt'
-#   output: float value for volume
-def get_volume():
+# FUNCTION: Takes incoming stream data and converts it to float values
+#   Output: a float that represents the volume of the mic input
+def process_volume():
     volume = 1
-    f = open(sound_FILE, "r")
-    try:
-        volume = f.readlines()[0]
-        volume = volume.strip()
-    except:
-        print("ERROR: VOLUME READ FAILURE")
-    f.close()
+    sensitivity = 12    # 12 is default
 
-    volume = float(volume)
-    if volume < 1:  volume = 1      # Volume input minimum of 1
+    # Takes the incoming data and converts it to volume data
+    indata, overflow = stream.read(CHUNK)
+    volume = np.linalg.norm(indata) * sensitivity
+    if volume < 1:  volume = 1          # Volume input minimum of 1
+    print("|" * int(volume))            # Volume terminal visual
     return volume
 
 
-
 # FUNCTION: loops through the frames of a video and applies the saturation aspect depending on volume
-#   Input:  Video file director
-def vidLoop(filepath):
-    stream.start()      # Start audio stream (stream) on the first loop
-    frames = get_next_frame(filepath)
-    frame = frames.__next__()
+def vidLoop():
+    stream.start()
+    frames = get_frames()
 
-    while(frames != None):
-        # Increases the saturation of frame by mic input value (volume) 
-        volume = get_volume()
+    while True:
+        frame = frames.__next__()
+        try:    frame.all()
+        except:
+            frames = get_frames()
+            frame = frames.__next__()
+            print("\nRESTART...\n")
+
+        volume = process_volume()
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         hsv[:,:,1] = hsv[:,:,1] * volume
         frame = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
 
         frame = cv2.resize(frame, (1280, 720))          # Resize frame
         cv2.imshow('sensoryProject', frame)             # Display frame
-        frame = frames.__next__()                       # Grab next frame
 
-        # if 'Esc' is hit, stop Audio Stream and exit program
         if cv2.waitKey(10) == 27:
             print("\nEXITING...")
             stream.stop()
             return
 
-    # RESTART the video if the end is reached
-    print("\nRESTART...\n")
-    vidLoop(filename)
-
-
-# FUNCTION:  processes the volumn input data
-#   Input: the mic input data for volumn
-def frame_callback(indata, frames, time, status):
-    sensitivity = 10    # 10 is default
-
-    # Takes the incoming data and converts it to volume data
-    volume_norm = np.linalg.norm(indata) * sensitivity
-
-    f = open(sound_FILE, "r+")      
-    f.write(str(volume_norm))                   # Stores volume data in 'sensoryProject.txt'
-    f.close()
-    print("|" * int(volume_norm))               # Volume terminal visual
-
 
 
 if __name__ == "__main__":
-    print("\tINITIALZING AUDIO STREAM...")
-    stream = sd.InputStream(        # Initializing audio stream
-        samplerate = 44100,         # Standard Sample Rate
-        blocksize = 512,            # Returns input at 512th frame
-        channels = 1,
-        device = sd.default.device,     # Default input device
-        callback = frame_callback)      # Volumn processing FUNCTION
-    print("\tAUDIO STREAM INITIALIZED!\n\n")
+    if sys.version[:1] != "3":
+        raise Exception("Python version is not 3.X (atleast 3.0)")
+
+    vid_FILE = find_Video()
+    stream = init_stream()
 
     cv2.namedWindow('sensoryProject', cv2.WINDOW_NORMAL)
-
-    print("\tPLAYING VIDEO...\n")
-    print("\tTo exit the program, press 'ESC' on the second window")
-    vidLoop(filename)
+    print("\tPLAYING VIDEO...")
+    print("\tTo exit the program, press 'ESC' on the second window\n")
+    vidLoop()
     
     cv2.destroyAllWindows()
